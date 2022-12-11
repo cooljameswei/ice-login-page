e2ef445d82d5e41afc18f26c3184cf64bd2d4371
leaderjohn <hopeyongjun421@gmail.com>
2022-12-11 01:50:38 -0500

    feat: LoginPage

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,18 @@ import logoimg from './assets/logo.png';
 function App() {
     return (
         <div className="App">
-            <div className="logo-img">
+            <div className="App-logo">
                 <img src={logoimg} />
+            </div>
+            <div className='App-login'>
+                <div>
+                    <h1 className="App-login-header">Welcome back!</h1>
+                </div>
+                <div className="App-login-input">
+                    <input className="App-login-input-form" type="text" name="address" placeholder="Email Address" />
+                    <input className="App-login-input-form" type="text" name="password" placeholder="Password" />
+                    <button className="App-login-input-button">Login</button>
+                </div>
             </div>
         </div>
     );
